d451525b916664295a23791f06967724801a3562
Teki <68933219+secondtime-github@users.noreply.github.com>
2023-03-31 03:13:56 +0900

    clear code

---
+++ ContributionGraph/ContributionGraph/View/ForTest/TodayView.swift
@@ -0,0 +1,66 @@
+//
+//  TodayView.swift
+//  ContributionGraph
+//
+//  Created by TEKI HOU on 2023-03-06.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    
+    @State private var currentDate = Date()
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Text(getContent(for: currentDate))
+                
+            }
+            .navigationTitle(
+                Text(dateFormatter.string(from: currentDate))
+                    .font(.headline)
+            )
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading:
+                    Button(action: {
+                        currentDate = Calendar.current.date(byAdding: .day, value: -1, to: currentDate)!
+                    }) {
+                        Image(systemName: "arrow.left")
+                    },
+                trailing:
+                    HStack {
+                        Button(action: {
+                            currentDate = Date()
+                        }) {
+                            Text("Today")
+                        }
+                        Button(action: {
+                            currentDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)!
+                        }) {
+                            Image(systemName: "arrow.right")
+                        }
+                    }
+            )
+        }
+    }
+    
+    func getContent(for date: Date) -> String {
+        return "Here is \(dateFormatter.string(from: date))"
+    }
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
+
+struct TodayView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayView()
+    }
+}
